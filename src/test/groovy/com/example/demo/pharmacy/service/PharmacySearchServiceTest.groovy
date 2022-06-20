07620ed5d8792f6e92d7675fa143cb87aec02bed
wonyong <zcx5674@gmail.com>
2022-06-20 23:40:06 +0900

    PharmacySearchServiceTest 테스트 코드 추가

--- src/test/groovy/com/example/demo/pharmacy/service/PharmacySearchServiceTest.groovy
+++ src/test/groovy/com/example/demo/pharmacy/service/PharmacySearchServiceTest.groovy
@@ -29,12 +29,6 @@ class PharmacySearchServiceTest extends Specification {
                         .pharmacyName("돌곶이온누리약국")
                         .latitude(37.61040424)
                         .longitude(127.0569046)
-                        .build(),
-                Pharmacy.builder()
-                        .id(3L)
-                        .pharmacyName("홀리데이약국")
-                        .latitude(37.60101417)
-                        .longitude(127.0331664)
                         .build()
         )
     }
@@ -42,6 +36,15 @@ class PharmacySearchServiceTest extends Specification {
     def "searchPharmacyList 입력 위, 경도 기준으로 거리순 정렬이 되는지 확인"() {
 
         given:
+        pharmacyList.add(
+                Pharmacy.builder()
+                        .id(3L)
+                        .pharmacyName("홀리데이약국")
+                        .latitude(37.60101417)
+                        .longitude(127.0331664)
+                        .build()
+        )
+
         // 서울 성북구 종암로10길
         double inputLatitude = 37.5960650456809
         double inputLongitude = 127.037033003036
@@ -63,7 +66,30 @@ class PharmacySearchServiceTest extends Specification {
         String.format("%.1f", results.get(2).getSecond()) == "2.4"
     }
 
-    def "searchPharmacyList 정해진 반경 10km 내에 "() {
+    def "searchPharmacyList 정해진 반경 10km 내에 검색이 되는지 확인"() {
+        given:
+        pharmacyList.add(
+                Pharmacy.builder()
+                        .id(3L)
+                        .pharmacyName("경기약국")
+                        .latitude(37.3825107393401)
+                        .longitude(127.236707811313)
+                        .build()
+        )
 
+        // 서울 성북구 종암로10길
+        double inputLatitude = 37.5960650456809
+        double inputLongitude = 127.037033003036
+
+        when:
+        pharmacyRepositoryService.findAll() >> pharmacyList
+
+        def results =
+                pharmacySearchService.searchPharmacyList(inputLatitude, inputLongitude)
+
+        then:
+        results.size() == 2
+        results.get(0).getFirst().getId() == 1
+        results.get(1).getFirst().getId() == 2
     }
 }
