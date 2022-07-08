a6aa24f8e8e72f0b419bdee0fe40678311569b89
wonyong <zcx5674@gmail.com>
2022-07-08 16:40:41 +0900

    MockMvc 테스트 코드 추가

---
+++ src/test/groovy/com/example/demo/direction/controller/DirectionControllerTest.groovy
@@ -0,0 +1,44 @@
+package com.example.demo.direction.controller
+
+import com.example.demo.direction.entity.Direction
+import com.example.demo.direction.service.DirectionService
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import spock.lang.Specification
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
+
+class DirectionControllerTest extends Specification {
+
+    private MockMvc mockMvc
+    private DirectionService directionService = Mock()
+
+    def setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(new DirectionController(directionService))
+                .build()
+    }
+
+    def "GET /dir/{encodedId}"() {
+        given:
+        Direction direction = Direction.builder()
+                .targetAddress("address")
+                .targetLatitude(38.11)
+                .targetLongitude(128.11)
+                .build()
+
+        String redirectURL = "https://map.kakao.com/link/map/address,38.11,128.11"
+
+        when:
+        directionService.findById(_) >> direction
+        ResultActions result = mockMvc.perform(MockMvcRequestBuilders.get("/dir/{encodedId}", "r"))
+
+        then:
+        result.andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(redirectURL))
+                .andDo(print())
+    }
+}
--- src/test/groovy/com/example/demo/direction/controller/FormControllerTest.groovy
+++ src/test/groovy/com/example/demo/direction/controller/FormControllerTest.groovy
@@ -1,21 +1,74 @@
 package com.example.demo.direction.controller
 
+import com.example.demo.direction.dto.InputDto
+import com.example.demo.direction.dto.OutputDto
 import com.example.demo.pharmacy.service.PharmacyRecommendationService
+import org.assertj.core.util.Lists
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.MvcResult
 import org.springframework.test.web.servlet.ResultActions
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.web.filter.CharacterEncodingFilter
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper
 import spock.lang.Specification
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*
 
 class FormControllerTest extends Specification {
 
     private MockMvc mockMvc
     private PharmacyRecommendationService pharmacyRecommendationService = Mock()
+    private ObjectMapper objectMapper
+    private List<OutputDto> outputDtoList
 
     def setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new FormController(pharmacyRecommendationService)).build();
+        // FormController를 MockMvc 객체로 만든다.
+        mockMvc = MockMvcBuilders.standaloneSetup(new FormController(pharmacyRecommendationService))
+                .build()
+
+        outputDtoList = Lists.newArrayList(
+                OutputDto.builder()
+                        .pharmacyName("pharmacy1")
+                        .build(),
+                OutputDto.builder()
+                        .pharmacyName("pharmacy2")
+                        .build()
+        )
+
+        objectMapper = new ObjectMapper()
+    }
+
+    def "GET /"() {
+
+        expect:
+        // FormController 의 "/" URI를 get방식으로 호출
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk()) // 예상 값을 검증한다.
+                .andExpect(view().name("main"))
+                .andDo(log())
+    }
+
+    def "POST /search"() {
+        given:
+        InputDto inputDto = new InputDto()
+        inputDto.setAddress("서울 성북구")
+
+        when:
+        pharmacyRecommendationService.recommendPharmacyList(_) >> outputDtoList
+        ResultActions result = mockMvc.perform(
+                post("/search")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(inputDto)))
+
+        then:
+        result.andExpect(status().isOk())
+                .andExpect(view().name("output"))
+                .andExpect(model().attributeExists("outputFormList"))
+                .andExpect(model().attribute("outputFormList", outputDtoList))
+                .andDo(print())
     }
 }
