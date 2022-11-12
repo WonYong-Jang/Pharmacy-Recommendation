f00eb77b6c891258ce72aa27d16aa0d97a8c94df
wonyong <zcx5674@gmail.com>
2022-11-12 12:12:48 +0900

    formControllerTest 리펙토링

--- src/test/groovy/com/example/demo/direction/controller/FormControllerTest.groovy
+++ src/test/groovy/com/example/demo/direction/controller/FormControllerTest.groovy
@@ -1,12 +1,11 @@
 package com.example.demo.direction.controller
 
-import com.example.demo.direction.dto.InputDto
 import com.example.demo.direction.dto.OutputDto
 import com.example.demo.pharmacy.service.PharmacyRecommendationService
-import org.assertj.core.util.Lists
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.ResultActions
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import spock.lang.Specification
 
@@ -23,11 +22,12 @@ class FormControllerTest extends Specification {
     private List<OutputDto> outputDtoList
 
     def setup() {
-        // FormController를 MockMvc 객체로 만든다.
+        // FormController MockMvc 객체로 만든다.
         mockMvc = MockMvcBuilders.standaloneSetup(new FormController(pharmacyRecommendationService))
                 .build()
 
-        outputDtoList = Lists.newArrayList(
+        outputDtoList = new ArrayList<>()
+        outputDtoList.addAll(
                 OutputDto.builder()
                         .pharmacyName("pharmacy1")
                         .build(),
@@ -42,31 +42,31 @@ class FormControllerTest extends Specification {
         expect:
         // FormController 의 "/" URI를 get방식으로 호출
         mockMvc.perform(get("/"))
+                .andExpect(handler().handlerType(FormController.class))
+                .andExpect(handler().methodName("main"))
                 .andExpect(status().isOk()) // 예상 값을 검증한다.
                 .andExpect(view().name("main"))
                 .andDo(log())
     }
 
     def "POST /search"() {
+
         given:
-        String address = "서울 성북구 종암동"
-        InputDto inputDto = new InputDto()
-        inputDto.setAddress(address)
+        String inputAddress = "서울 성북구 종암동"
 
         when:
-        ResultActions result = mockMvc.perform(
-                post("/search")
-                        .contentType(MediaType.APPLICATION_FORM_URLENCODED) // @ModelAttribute 매핑 검증을 위한 content type 지정
-                        .content("address=서울 성북구 종암동"))
+        def resultActions = mockMvc.perform(post("/search")
+                .param("address", inputAddress))
 
         then:
-        1 * pharmacyRecommendationService.recommendPharmacyList({argument ->
-            assert argument == address // mock 객체의 argument 검증
+        1 * pharmacyRecommendationService.recommendPharmacyList(argument -> {
+            assert argument == inputAddress // mock 객체의 argument 검증
         }) >> outputDtoList
 
-        result.andExpect(status().isOk())
+        resultActions
+                .andExpect(status().isOk())
                 .andExpect(view().name("output"))
-                .andExpect(model().attributeExists("outputFormList"))
+                .andExpect(model().attributeExists("outputFormList")) // model에 outputFormList라는 key가 존재하는지 확인
                 .andExpect(model().attribute("outputFormList", outputDtoList))
                 .andDo(print())
     }
